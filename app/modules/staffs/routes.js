34e0caf64c62fd5e4e8ec0dfce4fa6c23de6ca08
rafhpabustan <rafhpabustan@gmail.com>
2018-08-19 17:02:25 +0800

    AUG 19

--- app/modules/staffs/routes.js
+++ app/modules/staffs/routes.js
@@ -20,20 +20,13 @@ function s_reports(req,res){
 function s_user(req,res){
     res.render('staffs/general/views/user');
 }
-function s_utils(req,res){
-    res.render('staffs/general/views/utils');
-}
+
 // TRANSACTIONS
 
-function s_classes(req,res){
-    res.render('staffs/transactions/views/t-classes');
-}
-function s_event(req,res){
-    res.render('staffs/transactions/views/t-event');
-}
+
 
 function s_freezed(req,res){
-    res.render('staff/transactions/views/t-freezed');
+    res.render('staffs/transactions/views/t-freezed');
 }
 
 function s_payment(req,res){
@@ -57,11 +50,6 @@ function s_regular(req,res){
 router.get('/', s_dash);
 router.get('/reports', s_reports);
 router.get('/user', s_user);
-router.get('/utils', s_utils);
-
-
-router.get('/classes', s_classes);
-router.get('/event', s_event);
 router.get('/freezed', s_freezed);
 router.get('/payment', s_payment);
 router.get('/pending', s_pending);
