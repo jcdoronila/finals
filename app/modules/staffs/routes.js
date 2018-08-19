d6e4fccb1802b0f88b96cc3d1dfdf3cf6adcb9d2
rafhpabustan <rafhpabustan@gmail.com>
2018-08-19 21:45:28 +0800

    AUG 19 2ND

--- app/modules/staffs/routes.js
+++ app/modules/staffs/routes.js
@@ -1,10 +1,11 @@
 var router = require('express').Router();
 var express = require('express');
 var authMiddleware = require('../auth/middlewares/auth');
-router.use(authMiddleware.staffHasAuth);
-
 var indexController = require('./controllers/index');
+
+router.use(authMiddleware.staffHasAuth);
 router.get('/', indexController);
+
 //LOGOUT
 function logout(req, res){
     res.render('auth/views/landing');
@@ -22,9 +23,6 @@ function s_user(req,res){
 }
 
 // TRANSACTIONS
-
-
-
 function s_freezed(req,res){
     res.render('staffs/transactions/views/t-freezed');
 }
@@ -41,6 +39,14 @@ function s_personal(req,res){
     res.render('staffs/transactions/views/t-personal');
 }
 
+function s_classes(req,res){
+    res.render('staffs/transactions/views/t-classes');
+}
+
+function s_event(req,res){
+    res.render('staffs/transactions/views/t-event');
+}
+
 function s_regular(req,res){
     res.render('staffs/transactions/views/t-regular');
 }
@@ -53,6 +59,8 @@ router.get('/user', s_user);
 router.get('/freezed', s_freezed);
 router.get('/payment', s_payment);
 router.get('/pending', s_pending);
+router.get('/classes', s_classes);
+router.get('/event', s_event);
 router.get('/personal', s_personal);
 router.get('/regular', s_regular);
 
